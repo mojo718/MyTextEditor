7a72de1484172c51c9eb3f5ac20f6f62063e03d4
mojo718 <153574491+mojo718@users.noreply.github.com>
2024-05-25 12:18:44 -0400

    modified root package.json to use concurrently and build out other scripts.  Modified Webpack.config.js to use required plug ins for service work\manifest file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+// Used class videos and https://developer.chrome.com/docs/workbox for reference
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
@@ -17,13 +18,53 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-      
+    plugins: 
+    [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E.'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'Notetaking app with JavaScript syntax highlighting',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          }
+        ]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test:/\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            }
+          }
+        }
       ],
     },
   };
